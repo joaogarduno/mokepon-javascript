656244ff2e07eae1b10ffcb841c14fd3ec309c14
Joao Garduño <joao.garduno@gmail.com>
2022-11-21 13:14:37 -0600

    Estructura 7: Ataques del Juego

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-// PEDIRLE AL NAVEGADOR QUE NOS AVISE CUANDO TODO EL CODIGO HTML YA HAYA CARGADO Y AHI DISPARAR LA FUNCION CON NUESTRO CÓDIGO JAVASCRIPT
-// ESO NOS AYUDA A QUE EL CÓDIGO O EL SCRIPT EL LLAMADO A NUESTRO ARCHIVO DE JAVASCRIPT PUEDA SEGUIR ARRIBA PERO EL CÓDIGO NO SE EJECUTE SI NO QUE HASTA QUE EL NAVEGADOR NOS AVISE QUE TODO ESTE HTML YA CARGO.
+// DECLARE GLOBAL VARIABLES
+let attackPlayer;
 
 function startToPlay(){
     // SELECCIONAMOS ELEMENTOS
     let btnPetsPlayer = document.querySelector('#btnPets');
-
     // Escuchar eventos
     btnPetsPlayer.addEventListener('click', selectPetsPlayer);
+
+    // --------------
+    // tenemos la logica en esta funcion de inicio
+    // variables de ataquees de jugadores
+    let btnFuego =  document.querySelector('#btnFire');
+    btnFuego.addEventListener('click', attackFire)
+
+    let btnAgua = document.querySelector('#btnWater');
+    btnAgua.addEventListener('click', attackWater)
+
+    let btnTierra = document.querySelector('#btnEarth');
+    btnTierra.addEventListener('click', attackEarth)
 }
 
 // FUNCCION PARA PODER ESCUCHAR EL EVENTO
+// seleccionando una mascota para el JUGADOR
 function selectPetsPlayer(){
     let inputSelect1 = document.querySelector('#hipodoge');
     let inputSelect2 = document.querySelector('#capipepo');
@@ -53,6 +65,23 @@ function selectPetEnemy(){
 
 }
 
+// logica para seleccionar los ataques:
+// funcion para seleccionar el tipo de ataque
+// Aqui van las funciones de los ataques segun cual fue el botón que dieron click
+function attackFire(){
+    attackPlayer = 'FUEGO'
+    alert
+}
+function attackWater(){
+    attackPlayer = 'AGUA'
+}
+function attackEarth(){
+    attackPlayer = 'TIERRA'
+}
+// function typeOfAttack(){
+//     let playerAttack = 
+// }
+
 // Accion que nos diga cual es la variable la mascota que seleccionaron los enemigo
 
 // FUNCION DE SELECCION ALEATORIO PARA EL RIVAL QUE ES LA CPU
@@ -60,5 +89,8 @@ function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+
+// PEDIRLE AL NAVEGADOR QUE NOS AVISE CUANDO TODO EL CODIGO HTML YA HAYA CARGADO Y AHI DISPARAR LA FUNCION CON NUESTRO CÓDIGO JAVASCRIPT
+// ESO NOS AYUDA A QUE EL CÓDIGO O EL SCRIPT EL LLAMADO A NUESTRO ARCHIVO DE JAVASCRIPT PUEDA SEGUIR ARRIBA PERO EL CÓDIGO NO SE EJECUTE SI NO QUE HASTA QUE EL NAVEGADOR NOS AVISE QUE TODO ESTE HTML YA CARGO.
 // Load the page
 window.addEventListener('load', startToPlay);
